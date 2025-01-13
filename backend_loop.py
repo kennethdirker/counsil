7dc4d2139f22e1be26c49ee17516e4344ac20bd6
Lau Bannenberg <lau.bannenberg@gmail.com>
2025-01-13 14:27:57 +0100

    refactor

--- backend_loop.py
+++ backend_loop.py
@@ -1,37 +1,46 @@
+import time
+
 from design_by_committee import db, app
 import sqlalchemy as sa
 from app.models import Discussion, User, Post
 
 
-def example():
-    # We need the app_context() to query the database
-    with app.app_context():
-        query = sa.select(Discussion)
-        discussions = db.session.scalars(query).all()
-    for d in discussions:
-        print(d.title)
-
-
-def contribute_to(dis):
+def contribute_to(discussion: Discussion, recent_posts):
     return "placeholder"
 
 
-if __name__ == '__main__':
-    # example()
+def progress(discussion: Discussion):
+    for member in discussion.assigned_users:
+        time.sleep(1)
+        if discussion.has_stalled():
+            break
+        recent_posts = discussion.get_posts_since_last_contribution(user_id=member.id,
+                                                                    discussion_id=discussion.id)
+        contribution = contribute_to(discussion, recent_posts)
+        post = Post(body=contribution, user_id=member.id, discussion_id=discussion.id, is_npc=True)
+        db.session.add(post)
+        db.session.commit()
+
+
+def conclude(discussion: Discussion):
+    pass
+
+
+def main():
     while True:
-        with app.app_context():
-            query = sa.select(Discussion)
-            discussions = db.session.scalars(query).all()
-            for discussion in discussions:
-                if discussion.state != 'RUNNING':
-                    continue
-                if discussion.has_stalled():
-                    continue
-                for member in discussion.assigned_users:
-                    if discussion.has_stalled():
-                        break
-                    recent_posts = discussion.get_posts_since_last_contribution(user_id=member.id, discussion_id=discussion.id)
-                    contribution = contribute_to(discussion)
-                    post = Post(body=contribution, user_id=member.id, discussion_id=discussion.id, is_npc=True)
-                    db.session.add(post)
-                    db.session.commit()
+        query = sa.select(Discussion)
+        discussions = db.session.scalars(query).all()
+        for discussion in discussions:
+            if discussion.state == 'SETUP':
+                continue
+            if discussion.has_stalled():
+                continue
+            if discussion.state == 'RUNNING':
+                progress(discussion)
+            if discussion.state == 'CONCLUDING':
+                conclude(discussion)
+
+
+if __name__ == '__main__':
+    with app.app_context():  # use Flasky stuff without running the frontend
+        main()

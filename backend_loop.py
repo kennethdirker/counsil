d7d5c4c3a0bea92572cb1d7748101fea6ff9ffea
Kenneth Dirker <kennethdirker@hotmail.nl>
2025-01-21 00:10:12 +0100

    game loop

--- backend_loop.py
+++ backend_loop.py
@@ -5,15 +5,11 @@ import sqlalchemy as sa
 from app.models import Discussion, User, Post
 from npc import NPC
 
-# from backend.agent_client import AgentCLient
-
-
-def contribute_to(discussion: Discussion, member: User, npc: NPC):
-    recent_posts = discussion.get_posts_since_last_contribution(user_id=member.id)
-    return "placeholder"
+from backend.agent_client import AgentClient
 
 
 def eagerness_to_contribute(member, discussion):
+    """"""
     last_post_id = db.session.scalar(
         sa.select(sa.func.max(Post.id))
         .where(Post.discussion_id == discussion.id)
@@ -40,15 +36,26 @@ def roulette_wheel_selection(members, discussion):
 
     # Step 3: Use the probability distribution to select a panel member
     chosen_member = random.choices(members, weights=probabilities, k=1)[0]
-
     return chosen_member
 
 
+def contribute_to(discussion: Discussion, member: User, npc: NPC):
+    """"""
+    recent_posts = discussion.get_posts_since_last_contribution(user_id=member.id)
+    recent_posts_bodies = [post.body for post in recent_posts]
+    print("recent_posts: ", )
+    with open("backend_loop.log", "a") as myfile:
+        myfile.write(f"recent posts:\n{recent_posts_bodies}\n\n")
+    s = npc.reconsider_viewpoint(recent_posts_bodies)
+    return s
+
+
 def progress(discussion: Discussion, room, client):
+    """"""
     # Check if NPCs are waiting for the human user to talk
     if discussion.has_stalled():
         return
-
+    print("progressing")
     member = roulette_wheel_selection(discussion.assigned_users, discussion)
     contribution = contribute_to(discussion, member, room[member.id])
     if contribution:
@@ -57,46 +64,59 @@ def progress(discussion: Discussion, room, client):
         db.session.commit()
 
 
-def conclude(discussion: Discussion):
-    pass
+def conclude(room: dict):
+# def conclude(discussion: Discussion, room,):
+    """"""
+    # Calculate the majority vote
+    print("Voting...")
+    accepted = [npc.vote() for npc in room.values()].count(True) > (len(room) / 2)
+    print(f"Votes counted... Vote {'accepted' if accepted else 'rejected'}!")
+    with open("backend_loop.log", "a") as myfile:
+        myfile.write(f"Concluding:\nVote accepted? {accepted}\n\n")
+    return accepted
 
-class NoneType:
-    pass
 
 def main():
+    print("Initializing agent client")
+    client = AgentClient()
+
     # Initialize chatroom dictionary to account for already existing chats
     print("Initializing chatrooms")
     rooms = {}
+    STATE_OPTIONS = ["RUNNING", "CONCLUDING"]
     query = sa.select(Discussion)
     discussions = db.session.scalars(query).all()
     for discussion in discussions:
-        STATE_OPTIONS = ["INITIALIZING", "RUNNING", "CONCLUDING"]
         if discussion.state in STATE_OPTIONS:
-            rooms[discussion.id] = {member.id: NPC(member, discussion) for member in discussion.assigned_users}
-
-    print("Initializing agent client")
-    # client = AgentCLient()
-    client = NoneType()
+            rooms[discussion.id] = {member.id: NPC(member, discussion, client, verbose = True) for member in discussion.assigned_users}
 
     # Main loop
-    print("Running discussions...")
+    print("Checking discussions...")
     while True:
-        # time.sleep(1)
+        time.sleep(1)
         query = sa.select(Discussion)
         discussions = db.session.scalars(query).all()
         for discussion in discussions:
+            print("Processing discussion ", discussion.id, discussion.state)
             if discussion.state == 'SETUP':
+                # Discussion starting state has not finalized yet...
                 continue
             if discussion.has_stalled():
+                # Wait for human to post in discussion...
                 continue
             if discussion.state == 'INITIALIZING':
-                rooms[discussion.id] = {member.id: NPC(member, discussion) for member in discussion.assigned_users}
+                # Discussion starting state has been defined
+                rooms[discussion.id] = {member.id: NPC(member, discussion, client, verbose = True) for member in discussion.assigned_users}
                 discussion.state = 'RUNNING'
                 db.session.commit()
             if discussion.state == 'RUNNING':
+                # Discussion is fully running
                 progress(discussion, rooms[discussion.id], client)
+                # All points have been made: Vote and finalize
             if discussion.state == 'CONCLUDING':
-                conclude(discussion)
+                conclude(discussion, rooms[discussion.id])
+                discussion.state = 'FINISHED'
+                db.sesseion.commit()
 
 
 if __name__ == '__main__':
--- npc.py
+++ npc.py
@@ -1,18 +1,75 @@
 from app.models import Discussion, User, Post
 import json
+from backend.agent_client import AgentClient
 
 
 class NPC:
-    def __init__(self, member: User, discussion: Discussion):
+    def __init__(
+            self, 
+            member: User, 
+            discussion: Discussion, 
+            client: AgentClient,
+            verbose = False
+        ):
         self.member_id = member.id
         self.about = json.loads(member.about_me)
-        self.viewpoint = self._form_viewpoint(discussion.posts)
-        self.arguments = self._prepare_arguments()
+        self.personality = self._assemble_personality()
+        self.proposal = discussion.get_subject()    # Includes setting
+        self.client = client
+        self.viewpoint = self._form_viewpoint()
+        if verbose:
+            with open("backend_loop.log", "a") as myfile:
+                myfile.write(f"Initial post body from {self.about['name']}:\n{self.viewpoint}")
 
-    def _form_viewpoint(self, posts):
-        # Generate a viewpoint about the discussion topic
-        return "placeholder"
 
-    def _prepare_arguments(self):
-        # As described by Mathijs: generate a number of arguments, then randomly discard some of them
-        return ['placeholder', 'placeholder']
+    def reconsider_viewpoint(self, posts: list[str]):
+        """
+        Adjust current viewpoint according to new information and NPC personality.
+        """
+        external = " ".join(posts)
+        summary = self.client.summarize(external)
+        self.viewpoint = self.client.adjust_opinion(
+            self.proposal,
+            self.personality,
+            self.viewpoint,
+            summary
+        )
+        return self.viewpoint
+    
+    def vote(self) -> bool:
+        """
+        Accept or reject the proposal. Return True if the proposal is accepted
+        or False otherwise.
+        """
+        return self.client.vote(
+            self.proposal,
+            self.viewpoint,
+            self.personality
+        )
+
+
+    def _assemble_personality(self):
+        info = self.about
+        personality = f"""
+You are roleplaying as a city counselor and will answer questions like a city counselor would. \
+Answer all questions considering the following personality: \
+Your name is {info["name"]}, aged 47 years old. You are a member of the city council \
+and are currently in a meeting to discuss a proposal about the city. \
+You live in the {info["lives in"]}. Your main mode of transport is \
+{info["main mode of transport"]} and your preferred mode of transport is \
+{info["preferred mode of transport"]}. You have \
+{"" if info["has kids"] else "no "}kids. Regarding city legislation, you \
+think that {info["main concern"]} is a very important subject, which you \
+are passionate about. Other legaslation issues that you think are \
+important to have in your city are: {", ".join(info["important issues"])}. \
+Issues that you are totally against are: {", ".join(info["is against"])}.
+        """ # ugly template
+        return personality
+
+
+    def _form_viewpoint(self):
+        """
+        Generate a initial viewpoint about the discussion topic        
+        """
+        self.viewpoint = self.client.form_opinion(self.proposal, self.personality)
+        return self.viewpoint
